084a26823e2f83c9153475751385125889657cfc
zouyuxin <yuxin.zou.stat@gmail.com>
2020-02-12 18:34:27 -0600

    fix bug

--- code/fit_susie.R
+++ code/fit_susie.R
@@ -12,7 +12,7 @@ for (r in 1:ncol(Y)) {
                                estimate_prior_variance=TRUE,
                                null_weight=null_weight,
                                coverage=0.95,min_abs_corr=0.5,
-                               tol=1e-3, s_init = s_init)
+                               tol=1e-3, s_init = s_init[[r]])
   } else {
       fitted[[r]] <- susie(X,Y[,r],
                                L=maxL,
@@ -21,7 +21,7 @@ for (r in 1:ncol(Y)) {
                                scaled_prior_variance=prior_var,
                                null_weight=null_weight,
                                coverage=0.95,min_abs_corr=0.5,
-                               tol=1e-3, s_init = s_init)
+                               tol=1e-3, s_init = s_init[[r]])
   }
   posterior[[r]] <- summary(fitted[[r]])
 }
